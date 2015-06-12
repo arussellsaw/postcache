039b48676d760d990306f7c6d4c961435a0fd41a
arussellsaw <alexrussellsaw@gmail.com>
2015-06-12 21:59:56 +0100

    fix tests

--- postcache_test.go
+++ postcache_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/arussellsaw/telemetry"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestNativeCache(t *testing.T) {
+	metrics["native.cache.items"] = telemetry.NewCurrent(tel, "native.cache.items", 0*time.Second)
+	metrics["native.cache.culls"] = telemetry.NewTotal(tel, "native.cache.culls", 0*time.Second)
 	cache := new(nativeCache)
 	cache.initialize()
 	lock, _ := cache.lock("464b88c2115e632e9711c9a66d27d705")
